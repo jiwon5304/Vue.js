7dc609cf2ef95f47096b1db152bef5965414f2b7
jiwon5304 <80395324+jiwon5304@users.noreply.github.com>
2022-03-03 18:32:38 +0900

    11. 싱글 파일 컴포넌트

--- vue-cli/src/App.vue
+++ vue-cli/src/App.vue
@@ -1,34 +1,34 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-    <!-- 컴포넌트 명명법 종류 -->
-    <!-- <hello-world></hello-world>
-    <HelloWorld></HelloWorld> 
-    <HelloWorld/> -->
-  </div>
+<div>
+  <!-- <app-header v-bind: 프롭스 속성 이름="상위 컴포넌트의 데이터 이름"></app-header> -->
+  <app-header 
+    v-bind:propsdata="str"
+    v-on:renew="renewStr"></app-header>
+  </div> 
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+//  컴포넌트의 내용을 정의해서 AppHeader라는 변수로 받아옴
+import AppHeader from './components/AppHeader.vue';
 
+// 컴포넌트에 있는 것을 가져다 사용할 확률이 높음
 export default {
-  // 인스턴스 옵션 속성 or 컴포넌트 옵션 속성
-  name: 'App',
+  data: function() {
+    return {
+      str: 'Header'
+    }
+  },
   components: {
-    HelloWorld,
-    // 'hello-world' : HelloWorld
+    'app-header': AppHeader
+  },
+  methods: {
+    renewStr: function() {
+      this.str = 'hi';
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
