16e2e22cc9d4b4abe6fa17f29365d261406505cb
jiwon <jiwon@jiwonui-MacBookAir.local>
2022-03-06 17:27:21 +0900

    12. 최종 프로젝트 - 사용자 입력 폼 만들기

---
+++ vue-form/src/App.vue
@@ -0,0 +1,51 @@
+<template>
+<form v-on:submit.prevent="submitForm">
+  <!-- v-model을 연결하면 개발자 도구 data에서 입력값을 들고옴 -->
+  <div>
+    <label for="username">id: </label>
+    <input id="username" type="text" v-model="username">
+  </div>
+  <div>
+    <label for="password">pw:</label>
+    <input id="password" type="password" v-model="password">
+  </div>
+
+  <button type="submit">login</button>
+</form>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  data: function(){
+    return{
+      username: '',
+      password: ''
+    }
+  },
+    methods: {  
+      // preventDefalut 는 새로고침을 하지 않음
+      submitForm: function() {
+        // event.preventDefault();
+        console.log(this.username, this.password);
+        var url = 'https://jsonplaceholder.typicode.com/users';
+        var data= {
+          username: this.username,
+          password: this.password
+        }
+        axios.post(url, data)
+        .then(function(response){
+          console.log(response);
+        })
+        .catch(function(error){
+          console.log(error);
+        })
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
